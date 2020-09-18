60b22178473dd9e9ff6ffdaaa5ba4ed01c2f194e
rezabh <bahadori.reza74@gmail.com>
2020-09-18 16:06:58 +0430

    add template login and change source code

--- account/views.py
+++ account/views.py
@@ -1,2 +1,15 @@
 from django.shortcuts import render
 from .models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from article.models import Article
+
+@login_required
+def home(request):
+    return render(request, 'registration/home.html')
+
+
+class ArticleList(LoginRequiredMixin, ListView):
+    template_name = 'registration/home.html'
+    queryset = Article.objects.all()
